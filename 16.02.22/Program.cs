dfc8c58ac554b2ce3c26f5e2e7078b83e490a6d9
GunelHH <gunelfh@code.edu.az>
2022-02-16 07:30:33 +0400

    New commit

---
+++ 16.02.22/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _16._02._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Aforizm = { "LearN FroM YesterdaY, LivE FoR TodaY, HopE FoR TomoRroW " };
+            SizeLetter(Aforizm);
+        }
+        public static void SizeLetter(string[] Aforizm)
+        {
+            foreach (string words in Aforizm)
+            {
+                foreach (char letter in words)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        Console.Write(char.ToUpper(letter));
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(letter));
+                    }
+                }
+            }
+        }
+    }
+}
